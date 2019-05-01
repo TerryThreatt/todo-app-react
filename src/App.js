cf177b602a51f622fff375305c179196b5660d3b
Terry Threatt <terry.threatt@gmail.com>
2019-05-01 18:54:08 -0500

    add crud functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
-import Todo from './components/Todo';
+
+function Todo({ todo, index, completeTodo, removeTodo }) { 
+  return ( 
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : ""}}
+    >
+      <div>
+      <button onClick={() => completeTodo(index)}>Complete</button>
+      </div> 
+      {todo.text}
+      <div> 
+        <button onClick={() => removeTodo(index)}>X</button>
+      </div>
+    </div>
+  );
+}
+
 
 function TodoForm({ addTodo }) { 
   const [value, setValue] = useState("");
@@ -11,27 +28,66 @@ function TodoForm({ addTodo }) {
     addTodo(value); 
     setValue("");
   };
-  
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="todo-input">
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      </div>
+    </form>
+  );
 }
 
 function App() {
   const [todos, setTodos] = useState([ 
-      { text: "Learn about React" }, 
-      { text: "Meet friend for lunch" }, 
-      { text: "Build really cool todo app" }
+      { text: "Learn about React", 
+        isCompleted: false
+      }, 
+      { text: "Meet friend for lunch",
+        isCompleted: false 
+      }, 
+      { text: "Build really cool todo app", 
+        isCompleted: false
+      }
     ]);
 
+  const addTodo = text => { 
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => { 
+    const newTodos = [...todos]; 
+    newTodos[index].isCompleted = true; 
+    setTodos(newTodos);
+
+  }; 
+
+  const removeTodo = index => { 
+    const newTodos = [...todos]; 
+    newTodos.splice(index, 1); 
+    setTodos(newTodos);
+  };
 
   return (
-    <div className="App">
+    <div className="app">
       <div className="todo-list">
+          <p className="todo-title">Todo</p>
           {todos.map((todo, index) => (
             <Todo 
               key={index}
               index={index}
               todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
             />
           ))}
+          <TodoForm addTodo={ addTodo } />
       </div>
     </div>
   );
